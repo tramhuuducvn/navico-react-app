156860474543d2dc705ace4d0ecc4ca4ab4d7daa
tramhuuducvn <tramhuuduc.vn@gmail.com>
2024-10-30 08:21:10 +0700

    add ant design

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, ColorPicker, ConfigProvider, Divider, Input, Space } from 'antd';
 
 function App() {
+  const [primary, setPrimary] = React.useState('#1677ff');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          🫶🫶🫶 Welcome to batdongday 🫶🫶🫶
-        </a>
-      </header>
-    </div>
+    // <ConfigProvider
+    //   theme={{
+    //     token: {
+    //       // Seed Token
+    //       colorPrimary: '#00b96b',
+    //       borderRadius: 2,
+
+    //       // Alias Token
+    //       colorBgContainer: '#f6ffed',
+    //     }
+    //   }}>
+    //   <Space>
+    //     <Button type="primary">Primary</Button>
+    //     <Button>Default</Button>
+    //   </Space>
+    // </ConfigProvider>
+    <>
+      <ColorPicker showText value={primary} onChange={(color) => setPrimary(color.toHexString())} />
+      <Divider />
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: primary,
+          },
+        }}
+      >
+        <Space>
+          <Input placeholder="Please Input" />
+          <Button type="primary">Submit</Button>
+        </Space>
+      </ConfigProvider>
+    </>
   );
 }
 
